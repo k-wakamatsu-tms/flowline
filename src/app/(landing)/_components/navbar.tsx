b895e43adbcb361cbdf2d57bb265197339567280
Wakamatsu Kouta <69659176+k-wakamatsu-tms@users.noreply.github.com>
2024-12-28 10:53:22 +0900

    Refactor Tailwind CSS configuration and update navbar text for localization
    - Enhanced Tailwind CSS configuration by adding container settings, keyframes, and animations for improved layout and visual effects.
    - Updated navbar component text from English to Japanese for better localization, changing "Sign in" to "ログイン" and "Get started" to "無料登録".

--- src/app/(landing)/_components/navbar.tsx
+++ src/app/(landing)/_components/navbar.tsx
@@ -6,16 +6,16 @@ import { Button } from "@/components/ui/button";
 export async function Navbar() {
   return (
     <nav className="fixed inset-x-0 flex h-20">
-      <div className="container flex w-full items-center justify-between  gap-4">
+      <div className="container flex w-full items-center justify-between gap-4">
         <h1 className="scroll-m-20 text-2xl font-semibold tracking-tight lg:text-2xl">
           Flowline
         </h1>
         <div className="space-x-4">
           <Button size="sm" variant="ghost" asChild>
-            <Link href={"/login"}>Sign in</Link>
+            <Link href={"/login"}>ログイン</Link>
           </Button>
           <Button size="sm" asChild>
-            <Link href={"/signup"}>Get started</Link>
+            <Link href={"/signup"}>無料登録</Link>
           </Button>
         </div>
       </div>
